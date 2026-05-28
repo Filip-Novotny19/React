463c43250280b87efe596e1c6ff65aa763392e34
FilipNovotny19 <novotfi@student.uzlabina.cz>
2026-05-28 09:41:35 +0200

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
             </Content>
             <Foot />
         </Layout>
+
         </div>
         </Router>
     );
--- src/components/pages/Login_page/Login_page.jsx
+++ src/components/pages/Login_page/Login_page.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
+import axios from 'axios';
 import '../../../styles/reset.css';
 import './Main.css';
 import { Layout, Input, Divider, Form, Modal } from 'antd';
 import { LockOutlined } from '@ant-design/icons';
-
 import { validateAndShow } from '../../../Modal.js';
 
 const { Content } = Layout;
@@ -15,50 +15,129 @@ function Login_page() {
     const handleOk = () => setIsModalOpen(false);
     const showModal = () => setIsModalOpen(true);
 
-    const onFinish = (values) => {
+    const onFinish = async (values) => {
         setFormData(values);
+
         validateAndShow(values, showModal);
+
+
+
+        const BASE_URL = 'http://localhost:3000';
+        const APP_ID = '85d6598db0bf3f62afd5db8507';
+        const config = {
+            headers:
+                {
+                    'Content-Type': 'application/json',
+                    'Accept-Language': 'cs'
+                }
+        }
+
+        try {
+            const authRes = await axios.post(`${BASE_URL}/tokens`,
+                {
+                    "setup": {
+                        "setup_id":"1",
+                        "language_id": "cs",
+                        "allowed_gps": "false",
+                        "allowed_notifications": false
+                    },
+                    "device": {
+                        "device_id": "my-unique-web-id-001",
+                        "device_type": "7",
+                        "device_system": "Web",
+                        "device_name": "Firefox"
+                    },
+                    "user": {
+                        "login": values.email,
+                        "password": values.password,
+                    },
+                    "properties": ["customer"],
+                    'X-External-App-Id': APP_ID
+                },
+
+                    config
+
+            );
+
+            const token = authRes.data.data.token_id;
+            console.log("Token acquired:", token);
+
+
+            const userLogin = await axios.post(`${BASE_URL}/tokens/${token}/actions/login`, {
+                    "login_type":"email",
+                    "login_value":values.email,
+                    "password":values.password,
+                    "X-External-App-Id": APP_ID
+                },
+
+                {
+                    config,
+                    headers: {
+                        "Authorization": "Bearer " + token,
+                    }
+
+                });
+
+            const customer = userLogin.data.data.customer_id;
+            console.log("customer ID:", customer);
+
+            const userRes = await axios.get(`${BASE_URL}/customers/${customer}`, {
+                config,
+                headers: {
+                    "Authorization": "Bearer " + token,
+                    'X-External-App-Id': APP_ID,
+                }
+            });
+
+
+
+
+            console.log("Customer information received");
+            console.dir(userRes.data.data.customers);
+
+        } catch (error) {
+            console.error("API Failed:");
+            console.error(error.response ? error.response.data : error.message);
+        }
+
     };
+
     return (
+        <Content id="content">
 
+            <Form layout="vertical" id="form" onFinish={onFinish}>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    className="input"
+                    rules={[
+                        { required: true, message: 'Nezadaný Email' },
+                        { type: 'email', message: 'Špatný formát emailu' }
+                    ]}
+                >
+                    <Input className="ins" placeholder="zadejte Váš Email" />
+                </Form.Item>
 
+                <Form.Item
+                    className="input"
+                    label="Heslo"
+                    name="password"
+                    rules={[{ required: true, message: 'Nezadané Heslo' }]}
+                >
+                    <Input.Password className="ins" placeholder="zadejte Vaše heslo" />
+                </Form.Item>
 
-            <Content id="content">
-                <Form layout="vertical" id="form" onFinish={onFinish}>
+                <a href="#"><LockOutlined /> Zapomenuté Heslo</a>
 
-                    <Form.Item
-                        label="Email"
-                        name="email"
-                        className="input"
-                        rules={[
-                            { required: true, message: 'Nezadaný Email' },
-                            { type: 'email', message: 'Špatný formát emailu' }
-                        ]}
-                    >
-                        <Input className="ins" placeholder="zadejte Váš Email" />
-                    </Form.Item>
+                <div id="choice">
 
-                    <Form.Item
-                        className="input"
-                        label="Heslo"
-                        name="password"
-                        rules={[{ required: true, message: 'Nezadané Heslo' }]}
-                    >
-                        <Input.Password className="ins" placeholder="zadejte Vaše heslo" />
-                    </Form.Item>
-
-                    <a href="#"><LockOutlined /> Zapomenuté Heslo</a>
-
-                    <div id="choice">
-                        <button type="submit" id="login">Přihlásit se</button>
-                        <Divider>nebo</Divider>
-                        <button type="button" id="signin">Vytvořit nový účet Moje Amber</button>
-                        <p className="mid-text">Jste náš stávající dárce a nemůžete se přihlásit?</p>
-                        <p className="mid-text"><a className="mid-text" href="#">Klikněte zde</a> a nastavte si heslo.</p>
-                    </div>
-                </Form>
-
-
+                    <button type="submit" id="login">Přihlásit se</button>
+                    <Divider>nebo</Divider>
+                    <button type="button" id="signin">Vytvořit nový účet Moje Amber</button>
+                    <p className="mid-text">Jste náš stávající dárce a nemůžete se přihlásit?</p>
+                    <p className="mid-text"><a className="mid-text" href="#">Klikněte zde</a> a nastavte si heslo.</p>
+                </div>
+            </Form>
 
             <Modal title="Vaše Informace" open={isModalOpen} onOk={handleOk} onCancel={handleOk}>
                 <div className="modal-par">
@@ -70,8 +149,7 @@ function Login_page() {
                     <p className="modal-p">{formData.password}</p>
                 </div>
             </Modal>
-            </Content>
-
+        </Content>
     );
 }
 
